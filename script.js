fb715264fa1b9f94493a345b12662e3a751bc758
Piotr-reader <piotr_partsianko@inbox.ru>
2022-11-24 21:01:27 +0300

    feat: implement btn

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
-const REQUEST_URL = 'https://fe.it-academy.by/Examples/test_JSE.json';
+const REQUEST_URL = "https://fe.it-academy.by/Examples/test_JSE.json";
 const SHR = new XMLHttpRequest();
-SHR.open('GET', REQUEST_URL);
+SHR.open("GET", REQUEST_URL);
 let arr;
 SHR.onload = () => {
-    arr = JSON.parse(SHR.response);
-    btn()
-}
+  arr = JSON.parse(SHR.response);
+  btn();
+};
 SHR.send();
 class PoemButton {
-    constructor(buttonCaption, alertText) {
-        this.buttonCaption = buttonCaption;
-        this.alertText = alertText;
-    }
-    createElem() {
-        let newEl = document.createElement('input');
-        newEl.classList.add('btn');
-        newEl.value = this.buttonCaption;
-        document.body.appendChild(newEl);
-      }
-
-    buttonPressed() {
-        return alert(this.alertText);
-    }
+  constructor(buttonCaption, alertText) {
+    this.buttonCaption = buttonCaption;
+    this.alertText = alertText;
+    let newEl = document.createElement("input");
+    newEl.classList.add("btn");
+    newEl.value = this.buttonCaption;
+    document.body.appendChild(newEl);
+  }
+  buttonPressed(text) {
+    return alert(text);
+  }
 }
 const btn = () => {
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i].buttonCaption;
-        new PoemButton(element).createElem();
-    }
-    const BTN = document.querySelectorAll('.btn');
-      BTN.forEach(btn => {
-        btn.addEventListener('click',(e) => {
-            let str = e.target.value;
-            str = +str[str.length - 1]
-            let text = new PoemButton(arr.buttonCaption, arr[str-1].alertText);
-            text.buttonPressed();
-            } );
-      })
-    };
-
-
-
+  let poem;
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i].buttonCaption;
+    const text = arr[i].alertText;
+    poem = new PoemButton(element, text);
+  }
+  const BTN = document.querySelectorAll(".btn");
+  BTN.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].buttonCaption === e.target.value) {
+          let text = arr[i].alertText;
+          poem.buttonPressed(text);
+        }
+      }
+    });
+  });
+};
