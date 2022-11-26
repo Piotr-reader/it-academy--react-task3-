5a3989f46f99829a739d41141851913257a7f3da
Piotr-reader <piotr_partsianko@inbox.ru>
2022-11-26 11:03:08 +0300

    feat: continue implement task

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const REQUEST_URL = "https://fe.it-academy.by/Examples/test_JSE.json";
 const SHR = new XMLHttpRequest();
 SHR.open("GET", REQUEST_URL);
-let arr;
 SHR.onload = () => {
-  arr = JSON.parse(SHR.response);
-  btn();
+  let arr = JSON.parse(SHR.response);
+  input(arr);
 };
 SHR.send();
 class PoemButton {
@@ -13,29 +12,28 @@ class PoemButton {
     this.alertText = alertText;
     let newEl = document.createElement("input");
     newEl.classList.add("btn");
+    newEl.type = "button";
     newEl.value = this.buttonCaption;
+    newEl.dataset.text = this.alertText;
     document.body.appendChild(newEl);
   }
-  buttonPressed(text) {
-    return alert(text);
+
+  buttonPressed() {
+    const BTN = document.querySelectorAll(".btn");
+    BTN.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        return alert(e.target.dataset.text);
+      });
+    });
   }
 }
-const btn = () => {
+
+const input = (arr) => {
   let poem;
   for (let i = 0; i < arr.length; i++) {
     const element = arr[i].buttonCaption;
     const text = arr[i].alertText;
     poem = new PoemButton(element, text);
   }
-  const BTN = document.querySelectorAll(".btn");
-  BTN.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i].buttonCaption === e.target.value) {
-          let text = arr[i].alertText;
-          poem.buttonPressed(text);
-        }
-      }
-    });
-  });
+  poem.buttonPressed();
 };
