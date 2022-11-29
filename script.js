b7b7a9ad7cc0b56d7e1109cef34e5bd04ef5427d
Piotr-reader <piotr_partsianko@inbox.ru>
2022-11-29 19:19:26 +0300

    feat:implement es6 syntax

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+"use strict";
 const REQUEST_URL = "https://fe.it-academy.by/Examples/test_JSE.json";
 const SHR = new XMLHttpRequest();
 SHR.open("GET", REQUEST_URL);
 SHR.onload = () => {
   let arr = JSON.parse(SHR.response);
-  input(arr);
+  inputCreate(arr);
 };
 SHR.send();
+
 class PoemButton {
   constructor(buttonCaption, alertText) {
     this.buttonCaption = buttonCaption;
@@ -14,26 +16,19 @@ class PoemButton {
     newEl.classList.add("btn");
     newEl.type = "button";
     newEl.value = this.buttonCaption;
-    newEl.dataset.text = this.alertText;
-    document.body.appendChild(newEl);
+    document.querySelector("#script").before(newEl);
+    newEl.addEventListener("click", () => {
+      this.buttonPressed();
+    });
   }
 
   buttonPressed() {
-    const BTN = document.querySelectorAll(".btn");
-    BTN.forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        return alert(e.target.dataset.text);
-      });
-    });
+    return alert(this.alertText);
   }
 }
 
-const input = (arr) => {
-  let poem;
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i].buttonCaption;
-    const text = arr[i].alertText;
-    poem = new PoemButton(element, text);
+const inputCreate = (arr) => {
+  for (let elem of arr) {
+    let obj = new PoemButton(elem.buttonCaption, elem.alertText);
   }
-  poem.buttonPressed();
 };
